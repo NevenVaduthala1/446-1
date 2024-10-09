b32af09288eeaba347bbda021765d49d4ac9d1b6
Neven Vaduthala <89496745+NevenVaduthala1@users.noreply.github.com>
2024-10-08 22:00:42 -0500

    Update finite.py

--- finite.py
+++ finite.py
@@ -148,93 +148,53 @@ class DifferenceUniformGrid(Difference):
 
         # assume constant grid spacing
         self.dx = grid.dx
-        
         i = np.arange(self.dof)[:, None]
         j = self.j[None, :]
         S = 1/factorial(i)*(j*self.dx)**i
 
         b = np.zeros( self.dof )
         b[self.derivative_order] = 1.
-        self.b = b
-        
+
         self.stencil = np.linalg.solve(S, b)
-        #print(self.stencil)
+
     def _build_matrix(self, grid):
         shape = [grid.N] * 2
         matrix = sparse.diags(self.stencil, self.j, shape=shape)
         matrix = matrix.tocsr()
-        j = self.j
+        jmin = -np.min(self.j)
+        if jmin > 0:
+            for i in range(jmin):
+                if isinstance(grid, UniformNonPeriodicGrid):
+                    # S[ii,jj] = (jj-i)^ii/(ii)!
+                    S = np.zeros((self.dof,self.dof))
+                    for ii in range(self.dof):
+                        for jj in range(self.dof):
+                            S[ii,jj] = 1/factorial(ii)*((jj-i)*self.dx)**ii
+                    b = np.zeros(self.dof)
+                    b[self.derivative_order] = 1.
+                    # nonperiodic stencil
+                    npstencil = np.linalg.solve(S,b)
+                    matrix[i,0:self.dof] = npstencil
+                else:
+                    matrix[i,-jmin+i:] = self.stencil[:jmin-i]
 
-        
-        if isinstance(grid, UniformNonPeriodicGrid):
-            rep = len(j)//2 
-            
-            #top row portion
-            s_new = np.zeros([len(j), len(j)])
-            for i in range(len(j)):
-                for k in range(len(j)):
-                    s_new[i,k] = ((k*self.dx)**i)/factorial(i)
-            a_s = np.linalg.solve(s_new,self.b)
-            matrix[0,:len(j)] = a_s
-
-            #last row
-
-            s_inv = np.copy(s_new[:,::-1])
-            
-            for i in range(len(j)):
-                s_inv[i] *= (-1)**i
-            
-            last_a_s = np.linalg.solve(s_inv, self.b)
-           
-            for i in range(len(last_a_s)):
-                matrix[-1, -i-1] = last_a_s[-i-1]
-            
-
-            # the rest of the rows
-            for i in range(1, rep):
-                appcol = np.zeros(len(j))
-                for jj in range(len(j)):
-                    appcol[jj] = ((-i*self.dx)**jj)/factorial(jj)
-                
-
-                s_mod = np.zeros((len(j), len(j)))
-                s_mod[:,0] = appcol
-
-                s_mod[:,1:] = s_new[:,:-1]
-                
-                a_s = np.linalg.solve(s_mod, self.b)
-                matrix[i,:len(j)] = a_s
-                s_new = np.copy(s_mod)
-                
-
-
-                #bottom rows
-                s_inv = np.copy(s_mod[:,::-1])
-                for k in range(len(j)):
-                    s_inv[k,:] *= (-1)**k
-                last_a_s = np.linalg.solve(s_inv, self.b)
-                for kk in range(len(last_a_s)):
-                    matrix[-i-1, -kk-1] = last_a_s[-kk-1]
-
-            
-
-        if isinstance(grid, UniformPeriodicGrid):
-            jmin = -np.min(self.j)
-            if jmin > 0:
-                for i in range(jmin):
-                    if isinstance(grid, UniformNonPeriodicGrid):
-                        pass
-                    else:
-                        matrix[i,-jmin+i:] = self.stencil[:jmin-i]
-
-            jmax = np.max(self.j)
-            if jmax > 0:
-                for i in range(jmax):
-                    if isinstance(grid, UniformNonPeriodicGrid):
-                        pass
-                    else:
-                        matrix[-jmax+i,:i+1] = self.stencil[-i-1:]
+        jmax = np.max(self.j)
+        if jmax > 0:
+            for i in range(jmax):
+                if isinstance(grid, UniformNonPeriodicGrid):
+                    S = np.zeros((self.dof,self.dof))
+                    for ii in range(self.dof):
+                        for jj in range(self.dof):
+                            S[ii,jj] = 1/factorial(ii)*(((grid.N-self.dof+jj)-(grid.N-jmax+i))*self.dx)**ii
+                    b = np.zeros(self.dof)
+                    b[self.derivative_order] = 1.
+                    # nonperiodic stencil
+                    npstencil = np.linalg.solve(S,b)
+                    matrix[-jmax+i,-self.dof:] = npstencil
+                else:
+                    matrix[-jmax+i,:i+1] = self.stencil[-i-1:]
         self.matrix = matrix
+
 
 class DifferenceNonUniformGrid(Difference):
 
@@ -355,48 +315,3 @@ class CenteredFiniteDifference4(Difference):
         matrix[0, -1] = -8/(12*h)
         matrix[1, -1] = 1/(12*h)
         self.matrix = matrix
-
-
-
-class BoundaryCondition:
-
-    def __init__(self, derivative_order, convergence_order, grid):
-        self.derivative_order = derivative_order
-        self.convergence_order = convergence_order
-        self.dof = self.derivative_order + self.convergence_order
-        self.grid = grid
-        self._build_vector()
-
-    def _coeffs(self, dx, j):
-        i = np.arange(self.dof)[:, None]
-        j = j[None, :]
-        S = 1/factorial(i)*(j*dx)**i
-
-        b = np.zeros( self.dof )
-        b[self.derivative_order] = 1.
-
-        return np.linalg.solve(S, b)
-
-
-class Left(BoundaryCondition):
-
-    def _build_vector(self):
-        dx = self.grid.dx
-        j = np.arange(self.dof)
-
-        coeffs = self._coeffs(dx, j)
-
-        self.vector = np.zeros(self.grid.N)
-        self.vector[:self.dof] = coeffs
-
-
-class Right(BoundaryCondition):
-
-    def _build_vector(self):
-        dx = self.grid.dx
-        j = np.arange(self.dof) - self.dof + 1
-
-        coeffs = self._coeffs(dx, j)
-
-        self.vector = np.zeros(self.grid.N)
-        self.vector[-self.dof:] = coeffs
