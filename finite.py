263639e1c020f9b7a644b9ad1e54715d33cfaa0a
Neven Vaduthala <89496745+NevenVaduthala1@users.noreply.github.com>
2024-10-10 09:50:29 -0500

    Update finite.py

--- finite.py
+++ finite.py
@@ -1,317 +1,196 @@
-from scipy.special import factorial
 import numpy as np
+from scipy.special import factorial
 from scipy import sparse
 
-def apply_matrix(matrix, array, axis, **kw):
-    """Contract any direction of a multidimensional array with a matrix."""
-    dim = len(array.shape)
-    # Build Einstein signatures
-    mat_sig = [dim, axis]
-    arr_sig = list(range(dim))
-    out_sig = list(range(dim))
-    out_sig[axis] = dim
-    # Handle sparse matrices
-    if sparse.isspmatrix(matrix):
-        matrix = matrix.toarray()
-    return np.einsum(matrix, mat_sig, array, arr_sig, out_sig, **kw)
+def apply_matrix(mat_data, arr_data, ax_idx, **kwargs):
+    dim_count = len(arr_data.shape)
+    mat_sig = [dim_count, ax_idx]
+    arr_sig = list(range(dim_count))
+    out_sig = list(range(dim_count))
+    out_sig[ax_idx] = dim_count
+    if sparse.isspmatrix(mat_data):
+        mat_data = mat_data.toarray()
+    return np.einsum(mat_data, mat_sig, arr_data, arr_sig, out_sig, **kwargs)
 
-def reshape_vector(data, dim=2, axis=-1):
-    """Reshape 1-dim array as a multidimensional vector."""
-    # Build multidimensional shape
-    shape = [1] * dim
-    shape[axis] = data.size
-    return data.reshape(shape)
+def reshape_vector(vec_data, dim_count=2, ax_idx=-1):
+    shape_vec = [1] * dim_count
+    shape_vec[ax_idx] = vec_data.size
+    return vec_data.reshape(shape_vec)
 
-def axindex(axis, index):
-    """Index array along specified axis."""
-    if axis < 0:
-        raise ValueError("`axis` must be positive")
-    # Add empty slices for leading axes
-    return (slice(None),)*axis + (index,)
-
-def axslice(axis, start, stop, step=None):
-    """Slice array along a specified axis."""
-    return axindex(axis, slice(start, stop, step))
 
 class UniformPeriodicGrid:
-
-    def __init__(self, N, length):
-        self.values = np.linspace(0, length, N, endpoint=False)
+    def __init__(self, grid_num, grid_len):
+        self.values = np.linspace(0, grid_len, grid_num, endpoint=False)
         self.dx = self.values[1] - self.values[0]
-        self.length = length
-        self.N = N
-
+        self.length = grid_len
+        self.N = grid_num
 
 class NonUniformPeriodicGrid:
+    def __init__(self, val_list, grid_len):
+        self.values = val_list
+        self.length = grid_len
+        self.N = len(val_list)
 
-    def __init__(self, values, length):
-        self.values = values
-        self.length = length
-        self.N = len(values)
+    def dx_array(self, stencil_vals):
+        shape_arr = (self.N, len(stencil_vals))
+        dx_matrix = np.zeros(shape_arr)
+        jmin = -np.min(stencil_vals)
+        jmax = np.max(stencil_vals)
 
-    def dx_array(self, j):
-        shape = (self.N, len(j))
-        dx = np.zeros(shape)
-
-        jmin = -np.min(j)
-        jmax = np.max(j)
-
-        values_padded = np.zeros(self.N + jmin + jmax)
+        padded_vals = np.zeros(self.N + jmin + jmax)
         if jmin > 0:
-            values_padded[:jmin] = self.values[-jmin:] - self.length
+            padded_vals[:jmin] = self.values[-jmin:] - self.length
         if jmax > 0:
-            values_padded[jmin:-jmax] = self.values
-            values_padded[-jmax:] = self.length + self.values[:jmax]
+            padded_vals[jmin:-jmax] = self.values
+            padded_vals[-jmax:] = self.length + self.values[:jmax]
         else:
-            values_padded[jmin:] = self.values
+            padded_vals[jmin:] = self.values
 
         for i in range(self.N):
-            dx[i, :] = values_padded[jmin+i+j] - values_padded[jmin+i]
+            dx_matrix[i, :] = padded_vals[jmin+i+stencil_vals] - padded_vals[jmin+i]
 
-        return dx
-
+        return dx_matrix
 
 class UniformNonPeriodicGrid:
-
-    def __init__(self, N, interval):
-        """ Non-uniform grid; no grid points at the endpoints of the interval"""
-        self.start = interval[0]
-        self.end = interval[1]
-        self.dx = (self.end - self.start)/(N-1)
-        self.N = N
-        self.values = np.linspace(self.start, self.end, N, endpoint=True)
+    def __init__(self, grid_num, interval_range):
+        self.start = interval_range[0]
+        self.end = interval_range[1]
+        self.dx = (self.end - self.start)/(grid_num-1)
+        self.N = grid_num
+        self.values = np.linspace(self.start, self.end, grid_num, endpoint=True)
 
 
+# Domain Class
 class Domain:
-
-    def __init__(self, grids):
-        self.dimension = len(grids)
-        self.grids = grids
-        shape = []
+    def __init__(self, grid_list):
+        self.dimension = len(grid_list)
+        self.grids = grid_list
+        shape_list = []
         for grid in self.grids:
-            shape.append(grid.N)
-        self.shape = shape
+            shape_list.append(grid.N)
+        self.shape = shape_list
 
     def values(self):
-        v = []
+        val_list = []
         for i, grid in enumerate(self.grids):
-            grid_v = grid.values
-            grid_v = reshape_vector(grid_v, self.dimension, i)
-            v.append(grid_v)
-        return v
+            grid_values = grid.values
+            grid_values = reshape_vector(grid_values, self.dimension, i)
+            val_list.append(grid_values)
+        return val_list
 
     def plotting_arrays(self):
-        v = []
+        val_list = []
         expanded_shape = np.array(self.shape, dtype=np.int)
         expanded_shape += 1
         for i, grid in enumerate(self.grids):
-            grid_v = grid.values
-            grid_v = np.concatenate((grid_v, [grid.length]))
-            grid_v = reshape_vector(grid_v, self.dimension, i)
-            grid_v = np.broadcast_to(grid_v, expanded_shape)
-            v.append(grid_v)
-        return v
+            grid_values = grid.values
+            grid_values = np.concatenate((grid_values, [grid.length]))
+            grid_values = reshape_vector(grid_values, self.dimension, i)
+            grid_values = np.broadcast_to(grid_values, expanded_shape)
+            val_list.append(grid_values)
+        return val_list
 
 
+# Difference classes and operators
 class Difference:
-
-    def __matmul__(self, other):
-        return apply_matrix(self.matrix, other, axis=self.axis)
+    def __matmul__(self, other_arr):
+        return apply_matrix(self.matrix, other_arr, ax_idx=self.axis)
 
 
 class DifferenceUniformGrid(Difference):
-
-    def __init__(self, derivative_order, convergence_order, grid, axis=0, stencil_type='centered'):
-        if stencil_type == 'centered' and convergence_order % 2 != 0:
-            raise ValueError("Centered finite difference has even convergence order")
-
+    def __init__(self, derivative_order, convergence_order, grid_inst, ax_idx=0, stencil_choice='centered'):
         self.derivative_order = derivative_order
         self.convergence_order = convergence_order
-        self.stencil_type = stencil_type
-        self.axis = axis
-        self._stencil_shape(stencil_type)
-        self._make_stencil(grid)
-        self._build_matrix(grid)
+        self.stencil_type = stencil_choice
+        self.axis = ax_idx
+        self._stencil_shape(stencil_choice)
+        self._make_stencil(grid_inst)
+        self._build_matrix(grid_inst)
 
-    def _stencil_shape(self, stencil_type):
-        dof = self.derivative_order + self.convergence_order
+    def _stencil_shape(self, stencil_choice):
+        dof_size = self.derivative_order + self.convergence_order
+        if stencil_choice == 'centered':
+            dof_size = dof_size - (1 - dof_size % 2)  # Ensure it's even
+            stencil_pos = np.arange(dof_size) - dof_size // 2
+        self.dof = dof_size
+        self.j = stencil_pos
 
-        if stencil_type == 'centered':
-            # cancellation if derivative order is even
-            dof = dof - (1 - dof % 2)
-            j = np.arange(dof) - dof//2
+    def _make_stencil(self, grid_inst):
+        self.dx = grid_inst.dx
+        i_vals = np.arange(self.dof)[:, None]
+        j_vals = self.j[None, :]
+        stencil_matrix = 1 / factorial(i_vals) * (j_vals * self.dx)**i_vals
 
-        self.dof = dof
-        self.j = j
+        b_vals = np.zeros(self.dof)
+        b_vals[self.derivative_order] = 1.0
+        self.stencil = np.linalg.solve(stencil_matrix, b_vals)
 
-    def _make_stencil(self, grid):
+    def _build_matrix(self, grid_inst):
+        shape_arr = [grid_inst.N] * 2
+        mat_instance = sparse.diags(self.stencil, self.j, shape=shape_arr)
+        mat_instance = mat_instance.tocsr()
 
-        # assume constant grid spacing
-        self.dx = grid.dx
-        i = np.arange(self.dof)[:, None]
-        j = self.j[None, :]
-        S = 1/factorial(i)*(j*self.dx)**i
-
-        b = np.zeros( self.dof )
-        b[self.derivative_order] = 1.
-
-        self.stencil = np.linalg.solve(S, b)
-
-    def _build_matrix(self, grid):
-        shape = [grid.N] * 2
-        matrix = sparse.diags(self.stencil, self.j, shape=shape)
-        matrix = matrix.tocsr()
         jmin = -np.min(self.j)
         if jmin > 0:
             for i in range(jmin):
-                if isinstance(grid, UniformNonPeriodicGrid):
-                    # S[ii,jj] = (jj-i)^ii/(ii)!
-                    S = np.zeros((self.dof,self.dof))
-                    for ii in range(self.dof):
-                        for jj in range(self.dof):
-                            S[ii,jj] = 1/factorial(ii)*((jj-i)*self.dx)**ii
-                    b = np.zeros(self.dof)
-                    b[self.derivative_order] = 1.
-                    # nonperiodic stencil
-                    npstencil = np.linalg.solve(S,b)
-                    matrix[i,0:self.dof] = npstencil
-                else:
-                    matrix[i,-jmin+i:] = self.stencil[:jmin-i]
+                mat_instance[i, -jmin+i:] = self.stencil[:jmin-i]
 
         jmax = np.max(self.j)
         if jmax > 0:
             for i in range(jmax):
-                if isinstance(grid, UniformNonPeriodicGrid):
-                    S = np.zeros((self.dof,self.dof))
-                    for ii in range(self.dof):
-                        for jj in range(self.dof):
-                            S[ii,jj] = 1/factorial(ii)*(((grid.N-self.dof+jj)-(grid.N-jmax+i))*self.dx)**ii
-                    b = np.zeros(self.dof)
-                    b[self.derivative_order] = 1.
-                    # nonperiodic stencil
-                    npstencil = np.linalg.solve(S,b)
-                    matrix[-jmax+i,-self.dof:] = npstencil
-                else:
-                    matrix[-jmax+i,:i+1] = self.stencil[-i-1:]
-        self.matrix = matrix
+                mat_instance[-jmax+i, :i+1] = self.stencil[-i-1:]
+
+        self.matrix = mat_instance
 
 
 class DifferenceNonUniformGrid(Difference):
-
-    def __init__(self, derivative_order, convergence_order, grid, axis=0, stencil_type='centered'):
-        if (derivative_order + convergence_order) % 2 == 0:
-            raise ValueError("The derivative plus convergence order must be odd for centered finite difference")
-
+    def __init__(self, derivative_order, convergence_order, grid_inst, ax_idx=0, stencil_choice='centered'):
         self.derivative_order = derivative_order
         self.convergence_order = convergence_order
-        self.stencil_type = stencil_type
-        self.axis = axis
-        self._stencil_shape(stencil_type)
-        self._make_stencil(grid)
-        self._build_matrix(grid)
+        self.stencil_type = stencil_choice
+        self.axis = ax_idx
+        self._stencil_shape(stencil_choice)
+        self._make_stencil(grid_inst)
+        self._build_matrix(grid_inst)
 
-    def _stencil_shape(self, stencil_type):
-        dof = self.derivative_order + self.convergence_order
-        j = np.arange(dof) - dof//2
-        self.dof = dof
-        self.j = j
+    def _stencil_shape(self, stencil_choice):
+        dof_size = self.derivative_order + self.convergence_order
+        stencil_pos = np.arange(dof_size) - dof_size // 2
+        self.dof = dof_size
+        self.j = stencil_pos
 
-    def _make_stencil(self, grid):
-        self.dx = grid.dx_array(self.j)
+    def _make_stencil(self, grid_inst):
+        self.dx = grid_inst.dx_array(self.j)
 
-        i = np.arange(self.dof)[None, :, None]
-        dx = self.dx[:, None, :]
-        S = 1/factorial(i)*(dx)**i
+        i_vals = np.arange(self.dof)[None, :, None]
+        dx_vals = self.dx[:, None, :]
+        stencil_matrix = 1 / factorial(i_vals) * (dx_vals)**i_vals
 
-        b = np.zeros( (grid.N, self.dof) )
-        b[:, self.derivative_order] = 1.
+        b_vals = np.zeros((grid_inst.N, self.dof))
+        b_vals[:, self.derivative_order] = 1.0
+        self.stencil = np.linalg.solve(stencil_matrix, b_vals)
 
-        self.stencil = np.linalg.solve(S, b)
-
-    def _build_matrix(self, grid):
-        shape = [grid.N] * 2
-        diags = []
+    def _build_matrix(self, grid_inst):
+        shape_arr = [grid_inst.N] * 2
+        diag_list = []
         for i, jj in enumerate(self.j):
             if jj < 0:
-                s = slice(-jj, None, None)
+                slice_vals = slice(-jj, None, None)
             else:
-                s = slice(None, None, None)
-            diags.append(self.stencil[s, i])
-        matrix = sparse.diags(diags, self.j, shape=shape)
+                slice_vals = slice(None, None, None)
+            diag_list.append(self.stencil[slice_vals, i])
+        mat_instance = sparse.diags(diag_list, self.j, shape=shape_arr)
+        mat_instance = mat_instance.tocsr()
 
-        matrix = matrix.tocsr()
         jmin = -np.min(self.j)
         if jmin > 0:
             for i in range(jmin):
-                matrix[i,-jmin+i:] = self.stencil[i, :jmin-i]
+                mat_instance[i, -jmin+i:] = self.stencil[i, :jmin-i]
 
         jmax = np.max(self.j)
         if jmax > 0:
             for i in range(jmax):
-                matrix[-jmax+i,:i+1] = self.stencil[-jmax+i, -i-1:]
+                mat_instance[-jmax+i, :i+1] = self.stencil[-jmax+i, -i-1:]
 
-        self.matrix = matrix
+        self.matrix = mat_instance
 
-
-class ForwardFiniteDifference(Difference):
-
-    def __init__(self, grid, axis=0):
-        self.axis = axis
-        h = grid.dx
-        N = grid.N
-        j = [0, 1]
-        diags = np.array([-1/h, 1/h])
-        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
-        matrix = matrix.tocsr()
-        matrix[-1, 0] = 1/h
-        self.matrix = matrix
-
-
-class CenteredFiniteDifference(Difference):
-
-    def __init__(self, grid, axis=0):
-        self.axis = axis
-        h = grid.dx
-        N = grid.N
-        j = [-1, 0, 1]
-        diags = np.array([-1/(2*h), 0, 1/(2*h)])
-        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
-        matrix = matrix.tocsr()
-        matrix[-1, 0] = 1/(2*h)
-        matrix[0, -1] = -1/(2*h)
-        self.matrix = matrix
-
-
-class CenteredFiniteSecondDifference(Difference):
-
-    def __init__(self, grid, axis=0):
-        self.axis = axis
-        h = grid.dx
-        N = grid.N
-        j = [-1, 0, 1]
-        diags = np.array([1/h**2, -2/h**2, 1/h**2])
-        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
-        matrix = matrix.tocsr()
-        matrix[-1, 0] = 1/h**2
-        matrix[0, -1] = 1/h**2
-        self.matrix = matrix
-
-
-class CenteredFiniteDifference4(Difference):
-
-    def __init__(self, grid, axis=0):
-        self.axis = axis
-        h = grid.dx
-        N = grid.N
-        j = [-2, -1, 0, 1, 2]
-        diags = np.array([1, -8, 0, 8, -1])/(12*h)
-        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
-        matrix = matrix.tocsr()
-        matrix[-2, 0] = -1/(12*h)
-        matrix[-1, 0] = 8/(12*h)
-        matrix[-1, 1] = -1/(12*h)
-
-        matrix[0, -2] = 1/(12*h)
-        matrix[0, -1] = -8/(12*h)
-        matrix[1, -1] = 1/(12*h)
-        self.matrix = matrix
